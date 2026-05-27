21918b9bd2217f8140faa8a96c51125357e8f569
Laura <laurallamasrod@gmail.com>
2026-05-27 20:04:55 +0200

    initial commit + palette

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return <nav></nav>
+}
+
+function Home() {
+  return <main></main>
+}
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
